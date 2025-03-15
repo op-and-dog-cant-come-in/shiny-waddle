01666a59259cea7e7b3a2e177c1fb1d21046a49b
wangyanzhan <14792755+wangyanzhan@user.noreply.gitee.com>
2025-03-15 17:04:38 +0800

    Changes

--- helloSpring/src/main/java/com/shinywaddle/hellospring/testproject/Controller/TestResponseBodyController.java
+++ helloSpring/src/main/java/com/shinywaddle/hellospring/testproject/Controller/TestResponseBodyController.java
@@ -1,16 +1,20 @@
 package com.shinywaddle.hellospring.testproject.Controller;
 
+import com.shinywaddle.hellospring.testproject.Service.TestHtmlService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class TestResponseBodyController {
+    @Autowired
+    public TestHtmlService testHtmlService;
 
     @GetMapping("test")
     public String testController() {
         log.info("testController has done!");
-        return "test";
+        return testHtmlService.testHtml();
     }
 }
---
+++ helloSpring/src/main/java/com/shinywaddle/hellospring/testproject/Service/TestHtmlService.java
@@ -0,0 +1,10 @@
+package com.shinywaddle.hellospring.testproject.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestHtmlService {
+    public String testHtml() {
+        return "testHtml";
+    }
+}
